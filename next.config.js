3e6481b07d5c06120ed2109783bd332e3f4d376e
Marvin Mensah <marvin.mensah@edu.devinci.fr>
2025-02-21 09:58:12 -0500

    internationalization test 2

--- next.config.js
+++ next.config.js
@@ -11,9 +11,18 @@ const nextConfig = {
     ]
   },
   i18n: {
-    locales: ['fr','en'], 
-    defaultLocale: 'fr',  // Remettre fr comme langue par défaut
-  }
-}
+    locales: ['fr', 'en'],
+    defaultLocale: 'fr',
+    localeDetection: {
+      // Permet de détecter la langue de l'utilisateur
+      // au premier chargement.
+      // IMPORTANT :  Ceci est la clé pour le premier chargement !
+      lookupCookie: 'NEXT_LOCALE', // Nom du cookie pour stocker la langue
+      lookupHeader: 'Accept-Language', // Pour les requêtes HTTP
+      lookupPath: true, // Pour les URLs (ex: /fr/accueil)
+      lookupQuerystring: 'lang', // Pour les paramètres d'URL (ex: ?lang=fr)
+    },
+  },
+};
 
 module.exports = nextConfig;
